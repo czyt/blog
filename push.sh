56713d835ea343b23e2a1f5b9e4ade32d741b18a
czyt <czyt@w.cn>
2024-04-28 20:50:52 +0800

    update

--- push.sh
+++ push.sh
@@ -1,7 +1,26 @@
-#!/usr/bin/bash
-echo pull latest
+#!/usr/bin/env bash
+
+echo "Pulling latest changes..."
 git pull
+
+git config user.email &>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Setting Git user email..."
+    git config --global user.email "czyt@w.cn"
+fi
+
+git config user.name &>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Setting Git user name..."
+    git config --global user.name "czyt"
+fi
+echo "Adding all changes..."
 git add .
+
+echo "Committing..."
 git commit -m "update"
+
+echo "Pushing to remote..."
 git push
-echo done
+
+echo "Done!"
